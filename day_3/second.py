a603333c0acbcc7a4eaef8662c0c74fe5248ba00
Sergio Cuenca <cuenken@gmail.com>
2025-12-06 16:10:32 +0100

    feat: day 3 part 2

--- day_3/second.py
+++ day_3/second.py
@@ -1,15 +1,28 @@
+from typing import Final
 
-import numpy as np
+JOLTAGE_DIGITS: Final[int] = 12
 
 def read_input() -> list[str]:
     with open('./input.txt', 'r') as file:
         return [line.strip() for line in file]
 
 def maximize_joltage(bank: list[str]):
-    v = np.array(bank, dtype=object)
-    joltage_matrix = v[:, None] + v[None, :]
-    accepted_values = np.triu(joltage_matrix, k=1)
-    return accepted_values.astype(int).max()
+    joltage = ""
+    start_sequence_idx = 0
+    for idx, i in enumerate(range(1, JOLTAGE_DIGITS + 1)):
+
+        # Shortening of the end sequence as the loop advances
+        end_sequence_idx = -JOLTAGE_DIGITS + idx + 1
+        if end_sequence_idx == 0:
+            end_sequence_idx = None
+
+        # The available numbers get shorter as the loop advances
+        available_numbers = bank[start_sequence_idx:end_sequence_idx]
+        max_value = max(map(int, available_numbers))
+        start_sequence_idx += available_numbers.index(str(max_value)) + 1
+        joltage += str(max_value)
+
+    return int(joltage)
 
 
 def solve():
